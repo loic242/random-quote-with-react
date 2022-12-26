653cd0880d03c16702794da3dc43cbcef6968524
loic242 <bmavah@gmail.com>
2022-12-26 17:01:48 +0200

    app done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [quote, setQuote] = useState("Hello");
-  const [auther, setAuther] = useState("World");
+  const [quote, setQuote] = useState(" ");
+  const [auther, setAuther] = useState(" ");
+
+  //API Integration
+  useEffect(
+    ()=>{
+      fetch("http://api.quotable.io/random")
+       .then(res => res.json())
+       .then(
+        (quote) => {
+          setQuote(quote.content);
+          setAuther(quote.author);
+          
+        }
+       )
+    }, []);
+
+    let newQote = () =>{
+      fetch("http://api.quotable.io/random")
+       .then(res => res.json())
+       .then(
+        (quote) => {
+          setQuote(quote.content);
+          setAuther(quote.author);
+          
+        })
+
+    }
   return (
     <div className="App" >
       <div className="quote" >
         <h2> {quote}</h2>
         <small>-{auther}-</small>
       </div><br />
-      <button className="btn">Generate New quote</button>
+      <button className="btn" onClick={newQote}>Generate New quote</button>
     </div>
   );
 }
