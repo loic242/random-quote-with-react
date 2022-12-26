694b9947a08d37ce7010580877d99c19f875ea28
loic242 <bmavah@gmail.com>
2022-12-26 16:12:56 +0200

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState("Hello");
+  const [auther, setAuther] = useState("World");
   return (
-    <div className="App">
-      <div className="quote">
-        <h2> New quote</h2>
-        <small>-Auther-</small>
-      </div>
+    <div className="App" >
+      <div className="quote" >
+        <h2> {quote}</h2>
+        <small>-{auther}-</small>
+      </div><br />
       <button className="btn">Generate New quote</button>
     </div>
   );
